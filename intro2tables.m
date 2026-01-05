2231cf99aa04233eb77374894e533b8047d2df70
Faisal Jan <faisaljanfazal@gmail.com>
2026-01-05 10:39:43 +0100

    Added Exercise solution for loading saved data

--- intro2tables.m
+++ intro2tables.m
@@ -5,3 +5,14 @@ rowNames = {'Girls', 'Boys'};
 columnNames = {'HighSchool', 'University'};
 
 Ktable = array2table(K, "RowNames",rowNames, "VariableNames", columnNames);
+
+%% loading saved data
+load citiesItaly.mat
+
+CIT = citiesItaly(end:-1:1, end:-1:1);
+
+CIT2 = citiesItaly + CIT;
+
+disp(citiesItaly([1, 3, end-2, end], :));
+disp(CIT([1, 3, end-2, end], :));
+disp(CIT2([1, 3, end-2, end], :))
